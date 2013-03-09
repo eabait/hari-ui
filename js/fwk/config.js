8f0449625330adc5669bc73a8c7839ba9cef80d5
eabait <estebanabait@gmail.com>
2013-03-09 12:08:40 -0300

    created an independent config file for requirejs

---
+++ js/fwk/config.js
@@ -0,0 +1,50 @@
+define(function() {
+  'use strict';
+
+  return {
+    //path aliases for loaded scripts
+    paths: {
+      'base.view' : 'fwk/core/base.view',
+      'container.view' : 'fwk/layoutmanagers/container.view',
+      'stacked.view' : 'fwk/layoutmanagers/stacked.view',
+      'list.view' : 'fwk/layoutmanagers/list.view',
+      'base.module' : 'fwk/core/base.module',
+      'cache' : 'fwk/transport/cache',
+      'backbone' : 'vendor/backbone/backbone-0.9.10',
+      'jquery' : 'vendor/jquery/jquery-1.8.0',
+      'transit' : 'vendor/jquery/plugins/jquery.transit',
+      'underscore' : 'vendor/underscore/lodash',
+      'handlebars' : 'vendor/handlebars/handlebars-1.0.0.beta.6',
+      'statemachine' : 'vendor/statemachine/Stately',
+      'modernizr' : 'vendor/modernizr/modernizr-latest',
+      'text' : 'vendor/require/plugins/text',
+      'pubsub' : 'vendor/pubsub/pubsub',
+      'lawnchair' : 'vendor/lawnchair/lawnchair-0.6.1',
+      'jst' : 'app/templates'
+    },
+    //load script's dependencies in correct order &
+    //wraps non-AMD scripts into AMD-modules
+    shim: {
+      'underscore' : {
+        exports: '_'
+      },
+      'backbone': {
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
+      },
+      'modernizr': {
+        exports : 'Modernizr'
+      },
+      'handlebars': {
+        exports : 'Handlebars'
+      },
+      'lawnchair' : {
+        exports: 'Lawnchair'
+      },
+      'transit': {
+        deps: ['jquery'],
+        exports : 'Transit'
+      }
+    }
+  };
+});
--- js/main.js
+++ js/main.js
@@ -3,59 +3,24 @@
  *
  * @author Esteban S. Abait <esteban.abait@globant.com>
  */
-require.config({
-  //path aliases for loaded scripts
-  paths: {
-    'base.view' : 'fwk/core/base.view',
-    'container.view' : 'fwk/layoutmanagers/container.view',
-    'stacked.view' : 'fwk/layoutmanagers/stacked.view',
-    'list.view' : 'fwk/layoutmanagers/list.view',
-    'base.module' : 'fwk/core/base.module',
-    'cache' : 'fwk/transport/cache',
-    'backbone' : 'vendor/backbone/backbone-0.9.10',
-    'jquery' : 'vendor/jquery/jquery-1.8.0',
-    'transit' : 'vendor/jquery/plugins/jquery.transit',
-    'underscore' : 'vendor/underscore/lodash',
-    'handlebars' : 'vendor/handlebars/handlebars-1.0.0.beta.6',
-    'statemachine' : 'vendor/statemachine/Stately',
-    'modernizr' : 'vendor/modernizr/modernizr-latest',
-    'text' : 'vendor/require/plugins/text',
-    'pubsub' : 'vendor/pubsub/pubsub',
-    'lawnchair' : 'vendor/lawnchair/lawnchair-0.6.1',
-    'jst' : 'app/templates'
-  },
-  //load script's dependencies in correct order &
-  //wraps non-AMD scripts into AMD-modules
-  shim: {
-    'underscore' : {
-      exports: '_'
-    },
-    'backbone': {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
-    'modernizr': {
-      exports : 'Modernizr'
-    },
-    'handlebars': {
-      exports : 'Handlebars'
-    },
-    'lawnchair' : {
-      exports: 'Lawnchair'
-    },
-    'transit': {
-      deps: ['jquery'],
-      exports : 'Transit'
-    }
-  }
-});
-
 require(
   [
-    'app/application.module'
+    'js/fwk/config.js'
   ],
-  function(Aplication) {
+  function(Config) {
     'use strict';
-    Aplication.start();
+
+    //1) Set requirejs configuration. This will set paths
+    //for all vendors and fwk dependencies/objects
+    require.config(Config);
+
+    //2) Start ApplicationModule
+    require(
+      [
+        'app/application.module'
+      ],
+      function(Application) {
+        Application.start();
+      });
   }
 );
